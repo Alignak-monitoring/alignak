1a4c7b889ef97c46a07b2b97022ff7039986321c
Grégory Starck <gregory.starck@savoirfairelinux.com>
2015-06-05 16:55:30 -0400

    Fix: test_scheduler_init: don't daemonize the arbiter
    because we do kill the proc pid at the end.

--- test/test_scheduler_init.py
+++ test/test_scheduler_init.py
@@ -67,6 +67,7 @@ daemons_config = {
 class testSchedulerInit(AlignakTest):
     def setUp(self):
         time_hacker.set_real_time()
+        self.arb_proc = None
 
     def create_daemon(self):
         cls = Alignak
@@ -85,7 +86,7 @@ class testSchedulerInit(AlignakTest):
         return data
 
     def tearDown(self):
-        proc = getattr(self, 'arb_proc', None)
+        proc = self.arb_proc
         if proc:
             self._get_subproc_data(proc)  # so to terminate / wait it..
 
@@ -110,7 +111,7 @@ class testSchedulerInit(AlignakTest):
             assert(fun in reg_list)
 
         # Launch an arbiter so that the scheduler get a conf and init
-        args = ["../alignak/bin/alignak_arbiter.py", "-c", daemons_config[Arbiter][0], "-d"]
+        args = ["../alignak/bin/alignak_arbiter.py", "-c", daemons_config[Arbiter][0]]
         proc = self.arb_proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         # Ok, now the conf
