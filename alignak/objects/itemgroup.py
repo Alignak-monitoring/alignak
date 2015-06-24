b1cc4bc78455ae09733aede392f087fff152cb61
David Durieux <d.durieux@siprossii.com>
2015-06-24 10:03:00 +0200

    Enh : Pylint - Add docstring in itemgroup.py

--- alignak/objects/itemgroup.py
+++ alignak/objects/itemgroup.py
@@ -52,6 +52,10 @@
 
 # And itemgroup is like a item, but it's a group of items :)
 
+"""
+This module provide Itemgroup and Itemgroups class used to define group of items
+"""
+
 from item import Item, Items
 
 from alignak.brok import Brok
@@ -59,9 +63,11 @@ from alignak.property import StringProp, ListProp, ToGuessProp
 from alignak.log import logger
 
 
-# TODO: subclass Item & Items for Itemgroup & Itemgroups?
 class Itemgroup(Item):
-
+    """
+    Class to manage a group of items
+    An itemgroup is used to regroup items (group)
+    """
     id = 0
 
     properties = Item.properties.copy()
@@ -94,9 +100,14 @@ class Itemgroup(Item):
             setattr(self, key, val)
 
 
-    # Copy the groups properties EXCEPT the members
-    # members need to be fill after manually
     def copy_shell(self):
+        """
+        Copy the groups properties EXCEPT the members.
+        Members need to be fill after manually
+
+        :return: Itemgroup object
+        :rtype: object
+        """
         cls = self.__class__
         old_id = cls.id
         new_i = cls()  # create a new group
@@ -114,10 +125,18 @@ class Itemgroup(Item):
         return new_i
 
     def replace_members(self, members):
+        """
+        Replace members of itemgroup by new members list
+
+        :param members: list of members
+        :type members: list
+        """
         self.members = members
 
-    # If a prop is absent and is not required, put the default value
     def fill_default(self):
+        """
+        Put property and it default value for properties not defined and not required
+        """
         cls = self.__class__
         for prop, entry in cls.properties.items():
             if not hasattr(self, prop) and not entry.required:
@@ -125,12 +144,24 @@ class Itemgroup(Item):
                 setattr(self, prop, value)
 
     def add_string_member(self, member):
+        """
+        Add new entry(member) to members list
+
+        :param member: member name
+        :type member: str
+        """
         add_fun = list.extend if isinstance(member, list) else list.append
         if not hasattr(self, "members"):
             self.members = []
         add_fun(self.members, member)
 
     def add_string_unknown_member(self, member):
+        """
+        Add new entry(member) to unknown members list
+
+        :param member: member name
+        :type member: str
+        """
         add_fun = list.extend if isinstance(member, list) else list.append
         if not self.unknown_members:
             self.unknown_members = []
@@ -145,9 +176,14 @@ class Itemgroup(Item):
         except ValueError:
             pass
 
-    # a item group is correct if all members actually exists,
-    # so if unknown_members is still []
     def is_correct(self):
+        """
+        Check if a group is valid.
+        Valid mean all members exists, so list of unknown_members is empty
+
+        :return: true if group is correct
+        :rtype: bool
+        """
         res = True
 
         if self.unknown_members:
@@ -165,12 +201,25 @@ class Itemgroup(Item):
 
 
     def has(self, prop):
+        """
+        Check if self have a property
+
+        :param prop: property name
+        :type prop: string
+        :return: true if self has this attribute
+        :rtype: bool
+        """
         return hasattr(self, prop)
 
 
-    # Get a brok with hostgroup info (like id, name)
-    # members is special: list of (id, host_name) for database info
     def get_initial_status_brok(self):
+        """
+        Get a brok with hostgroup info (like id, name)
+        Members contain list of (id, host_name)
+
+        :return:Brok object
+        :rtype: object
+        """
         cls = self.__class__
         data = {}
         # Now config properties
@@ -188,18 +237,39 @@ class Itemgroup(Item):
 
 
 class Itemgroups(Items):
+    """
+    Class to manage list of groups of items
+    An itemgroups is used to regroup items group
+    """
 
-    # If a prop is absent and is not required, put the default value
     def fill_default(self):
+        """
+        Put property and it default value for properties not defined and not required in
+        each itemgroup
+        """
         for i in self:
             i.fill_default()
 
 
     def add(self, ig):
+        """
+        Add an item (itemgroup) to Itemgroups
+
+        :param ig: an item
+        :type ig:
+        """
         self.add_item(ig)
 
 
     def get_members_by_name(self, gname):
+        """
+        Get members of groups have name in parameter
+
+        :param gname: name of group
+        :rtype gname: str
+        :return: list of members
+        :rtype: list
+        """
         g = self.find_by_name(gname)
         if g is None:
             return []
