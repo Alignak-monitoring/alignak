ad7735d67529793231714a1ecd8cb23fcb984e25
Sebastien Coavoux <sebastien.coavoux@savoirfairelinux.com>
2015-07-07 09:26:38 -0400

    Enh: Pylint - Docstring in acknowledge.py

--- alignak/acknowledge.py
+++ alignak/acknowledge.py
@@ -46,6 +46,10 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
+"""This module provides Acknowledge class that
+implements acknowledgment for notification. Basically used for parsing.
+
+"""
 
 
 class Acknowledge:
