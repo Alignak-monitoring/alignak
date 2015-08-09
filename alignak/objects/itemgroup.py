ad0634ac8fa8f7d326402e7b38e5dee716807c6b
Grégory Starck <gregory.starck@savoirfairelinux.com>
2015-08-09 15:26:54 -0400

    Clean: deprecate Itemgroup.has(),
    we are not Java,
    directly use hasattr(your_item_group, some_attr) instead.

--- alignak/objects/itemgroup.py
+++ alignak/objects/itemgroup.py
@@ -55,6 +55,7 @@
 """
 This module provide Itemgroup and Itemgroups class used to define group of items
 """
+import warnings
 
 from alignak.objects.item import Item, Items
 
@@ -191,6 +192,11 @@ class Itemgroup(Item):
         :return: True if self has this attribute, otherwise False
         :rtype: bool
         """
+        warnings.warn(
+            "{s.__class__.__name__} is deprecated, please use "
+            "`hasattr(your_object, attr)` instead. This has() method will "
+            "be removed in a later version.".format(s=self),
+            DeprecationWarning, stacklevel=2)
         return hasattr(self, prop)
 
     def get_initial_status_brok(self):
