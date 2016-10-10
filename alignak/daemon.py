9eb740cffac29af5137afe96ca140ac1fd0d0d11
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-10-10 06:54:20 +0200

    Fix pylint

--- alignak/daemon.py
+++ alignak/daemon.py
@@ -647,12 +647,10 @@ class Daemon(object):  # pylint: disable=R0902
         manager.start()
         return manager
 
-    def do_daemon_init_and_start(self, fake=False):
+    def do_daemon_init_and_start(self):
         """Main daemon function.
         Clean, allocates, initializes and starts all necessary resources to go in daemon mode.
 
-        :param fake: use for test to do not launch runonly feature, like the stats reaper thread.
-        :type fake: bool
         :return: None
         """
         self.change_to_user_group()
