b6eec08ed16e1a2af1be4c9797beee0d7834fa14
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-10-26 18:23:07 +0200

    Add tests for log events

--- test/test_parse_logevent.py
+++ test/test_parse_logevent.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright (C) 2015-2016: Alignak team, see AUTHORS.txt file for contributors
+# Copyright (C) 2015-2015: Alignak team, see AUTHORS.txt file for contributors
 #
 # This file is part of Alignak.
 #
@@ -112,14 +112,29 @@ class TestParseLogEvent(AlignakTest):
         self.assertEqual(event.data, expected)
 
     def test_downtime_alert_host(self):
-        log = '[1279250211] HOST DOWNTIME ALERT: maast64;STARTED; Host has entered a period of scheduled downtime'
+        log = '[1279250211] HOST DOWNTIME ALERT: testhost;STARTED; Host has entered a period of scheduled downtime'
         expected = {
             'event_type': 'DOWNTIME',
-            'hostname': 'maast64',
+            'hostname': 'testhost',
+            'service_desc': None,
             'state': 'STARTED',
             'time': 1279250211,
             'output': ' Host has entered a period of scheduled downtime',
             'downtime_type': 'HOST'
+        }
+        event = LogEvent(log)
+        self.assertEqual(event.data, expected)
+
+    def test_downtime_alert_service(self):
+        log = '[1279250211] SERVICE DOWNTIME ALERT: testhost;check_ssh;STARTED; Service has entered a period of scheduled downtime'
+        expected = {
+            'event_type': 'DOWNTIME',
+            'hostname': 'testhost',
+            'service_desc': 'check_ssh',
+            'state': 'STARTED',
+            'time': 1279250211,
+            'output': ' Service has entered a period of scheduled downtime',
+            'downtime_type': 'SERVICE'
         }
         event = LogEvent(log)
         self.assertEqual(event.data, expected)
