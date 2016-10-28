5daae907ca723eb9b467e716778b549819f7baaa
David Durieux <d.durieux@siprossii.com>
2016-10-28 10:47:35 +0200

    Fix

--- test/test_ssl.py
+++ test/test_ssl.py
@@ -60,14 +60,14 @@ class TestSsl(AlignakTest):
         except ImportError:
             self.ssl_installed = False
             print "Install pyopenssl"
-            subprocess.call(["sudo", "pip", "install", "--upgrade", "pyopenssl"])
+            subprocess.call(["sudo", "pip", "install", "--upgrade", "pyopenssl"], shell=True)
 
     def tearDown(self):
         for name, proc in self.procs.items():
             if proc:
                 self._get_subproc_data(name)  # so to terminate / wait it..
         if not self.ssl_installed:
-            subprocess.call(["sudo", "pip", "uninstall", "pyopenssl"])
+            subprocess.call(["sudo", "pip", "uninstall", "pyopenssl"], shell=True)
 
     def test_ssl_satellites(self):
         """
