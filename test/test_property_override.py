6f38d1a4968f3b12329e13ec8a934bf0c59408f1
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-11-09 18:01:33 +0100

    Add tests for properties override

--- test/test_property_override.py
+++ test/test_property_override.py
@@ -54,23 +54,27 @@ from alignak_test import unittest, AlignakTest
 class TestPropertyOverride(AlignakTest):
 
     def setUp(self):
-        self.setup_with_file(['etc/alignak_property_override.cfg'])
-
+        self.setup_with_file('cfg/cfg_property_override.cfg')
+        self.assertTrue(self.conf_is_correct)
+        self._sched = self.schedulers['scheduler-master'].sched
+        
     def test_service_property_override(self):
-        svc1 = self.sched.services.find_srv_by_name_and_hostname("test_host_01", "srv-svc")
-        svc2 = self.sched.services.find_srv_by_name_and_hostname("test_host_02", "srv-svc")
-        svc1proc1 = self.sched.services.find_srv_by_name_and_hostname("test_host_01", "proc proc1")
-        svc1proc2 = self.sched.services.find_srv_by_name_and_hostname("test_host_01", "proc proc2")
-        svc2proc1 = self.sched.services.find_srv_by_name_and_hostname("test_host_02", "proc proc1")
-        svc2proc2 = self.sched.services.find_srv_by_name_and_hostname("test_host_02", "proc proc2")
-        tp24x7 = self.sched.timeperiods.find_by_name("24x7")
-        tptest = self.sched.timeperiods.find_by_name("testperiod")
-        cgtest = self.sched.contactgroups.find_by_name("test_contact")
-        cgadm = self.sched.contactgroups.find_by_name("admins")
-        cmdsvc = self.sched.commands.find_by_name("check_service")
-        cmdtest = self.sched.commands.find_by_name("dummy_command")
-        svc12 = self.sched.services.find_srv_by_name_and_hostname("test_host_01", "srv-svc2")
-        svc22 = self.sched.services.find_srv_by_name_and_hostname("test_host_02", "srv-svc2")
+        """ Property override """
+        svc1 = self._sched.services.find_srv_by_name_and_hostname("test_host_01", "srv-svc")
+        svc2 = self._sched.services.find_srv_by_name_and_hostname("test_host_02", "srv-svc")
+        svc1proc1 = self._sched.services.find_srv_by_name_and_hostname("test_host_01", "proc proc1")
+        svc1proc2 = self._sched.services.find_srv_by_name_and_hostname("test_host_01", "proc proc2")
+        svc2proc1 = self._sched.services.find_srv_by_name_and_hostname("test_host_02", "proc proc1")
+        svc2proc2 = self._sched.services.find_srv_by_name_and_hostname("test_host_02", "proc proc2")
+        tp24x7 = self._sched.timeperiods.find_by_name("24x7")
+        tp_none = self._sched.timeperiods.find_by_name("none")
+        tptest = self._sched.timeperiods.find_by_name("testperiod")
+        cgtest = self._sched.contactgroups.find_by_name("test_contact")
+        cgadm = self._sched.contactgroups.find_by_name("admins")
+        cmdsvc = self._sched.commands.find_by_name("check_service")
+        cmdtest = self._sched.commands.find_by_name("dummy_command")
+        svc12 = self._sched.services.find_srv_by_name_and_hostname("test_host_01", "srv-svc2")
+        svc22 = self._sched.services.find_srv_by_name_and_hostname("test_host_02", "srv-svc2")
 
         # Checks we got the objects we need
         self.assertIsNot(svc1, None)
@@ -91,37 +95,47 @@ class TestPropertyOverride(AlignakTest):
         # Check non overriden properies value
         for svc in (svc1, svc1proc1, svc1proc2, svc2proc1, svc12):
             self.assertEqual(["test_contact"], svc.contact_groups)
-            self.assertIs(tp24x7.uuid, svc.maintenance_period)
+            self.assertEqual(self._sched.timeperiods[tp24x7.uuid].get_name(),
+                             self._sched.timeperiods[svc.maintenance_period].get_name())
             self.assertEqual(1, svc.retry_interval)
-            self.assertIs(cmdsvc, svc.check_command.command)
+            self.assertIs(self._sched.commands[cmdsvc.uuid],
+                          self._sched.commands[svc.check_command.command.uuid])
             self.assertEqual(["w","u","c","r","f","s"], svc.notification_options)
             self.assertIs(True, svc.notifications_enabled)
 
         # Check overriden properies value
         for svc in (svc2, svc2proc2, svc22):
             self.assertEqual(["admins"], svc.contact_groups)
-            self.assertIs(tptest.uuid, svc.maintenance_period)
+            self.assertEqual(self._sched.timeperiods[tptest.uuid].get_name(),
+                             self._sched.timeperiods[svc.maintenance_period].get_name())
             self.assertEqual(3, svc.retry_interval)
-            self.assertIs(cmdtest, svc.check_command.command)
+            self.assertIs(self._sched.commands[cmdtest.uuid],
+                          self._sched.commands[svc.check_command.command.uuid])
             self.assertEqual(["c","r"], svc.notification_options)
             self.assertIs(False, svc.notifications_enabled)
 
 
-class TestConfigBroken(AlignakTest):
-
-    def setUp(self):
-        self.setup_with_file(['etc/alignak_property_override_broken.cfg'])
+class TestPropertyOverrideConfigBroken(AlignakTest):
 
     def test_service_property_override_errors(self):
-        self.assertFalse(self.conf.conf_is_correct)
+        """ Property override broken """
+        with self.assertRaises(SystemExit):
+            self.setup_with_file('cfg/cfg_property_override_broken.cfg')
+        self.assertFalse(self.conf_is_correct)
 
-        # Get the arbiter's log broks
-        [b.prepare() for b in self.broks.values()]
-        logs = [b.data['log'] for b in self.broks.values() if b.type == 'log']
-
-        self.assertEqual(1, len([log for log in logs if re.search('Error: invalid service override syntax: fake', log)]) )
-        self.assertEqual(1, len([log for log in logs if re.search("Error: trying to override property 'retry_interval' on service 'fakesrv' but it's unknown for this host", log)]) )
-        self.assertEqual(1, len([log for log in logs if re.search("Error: trying to override 'host_name', a forbidden property for service 'proc proc2'", log)]) )
+        self.assertIn("Configuration in host::test_host_02 is incorrect; "
+                      "from: cfg/default/daemons/reactionner-master.cfg:55",
+                      self.configuration_errors)
+        self.assertIn("Error: invalid service override syntax: fake value",
+                      self.configuration_errors)
+        self.assertIn("Error: trying to override property 'retry_interval' "
+                      "on service 'fakesrv' but it's unknown for this host",
+                      self.configuration_errors)
+        self.assertIn("Error: trying to override 'host_name', a forbidden property "
+                      "for service 'proc proc2'",
+                      self.configuration_errors)
+        self.assertIn("hosts configuration is incorrect!",
+                      self.configuration_errors)
 
 
 if __name__ == '__main__':
