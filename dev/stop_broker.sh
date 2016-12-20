b78320af7f4f7bc224445704f22bd693138ddeb1
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-12-20 07:02:58 +0100

    Update alignak installation process Update setup for packaging build Remove pbr from the project dependencies (as of https://github.com/Alignak-monitoring/alignak-packaging/issues/24) Set alignak directories according to packaging choice Update paths.cfg configuration file Update dev launch script

--- dev/stop_broker.sh
+++ dev/stop_broker.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 #
 # Copyright (C) 2015-2016: Alignak team, see AUTHORS.txt file for contributors
 #
@@ -17,47 +18,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Alignak.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-# This file incorporates work covered by the following copyright and
-# permission notice:
-#
-#  Copyright (C) 2009-2014:
-#     Gabes Jean, naparuba@gmail.com
-#     Gerhard Lausser, Gerhard.Lausser@consol.de
-#     Gregory Starck, g.starck@gmail.com
-#     Hartmut Goebel, h.goebel@goebel-consult.de
-#
-#  This file is part of Shinken.
-#
-#  Shinken is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Shinken is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
-
-
 DIR="$(cd $(dirname "$0"); pwd)"
-BIN="$DIR"/../bin
-ETC="$DIR"/../etc
 
-echo "Stopping broker"
+DAEMON_TYPE="BROKER"
+DAEMON_NAME="${DAEMON_TYPE}_MASTER"
 
-parent=$(cat /var/run/alignak/brokerd.pid)
-
-kill $parent
-sleep 1
-
-# kill parent and childs broker processes
-for brokerpid in $(ps -f --ppid $parent | grep "alignak-broker" | awk '{print $2}')
-do
-    echo "KILLING MODULE BROKER PROCESS" $brokerpid
-    kill $brokerpid
-done
+"$DIR/_stop_daemon.sh" $@ "$DAEMON_NAME"
 
