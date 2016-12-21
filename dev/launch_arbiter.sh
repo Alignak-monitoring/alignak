e9c12c42ff0088b2145b4fd52c1456699d0ebcf3
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-12-21 12:12:16 +0100

    #656: alignak.ini and start/stop scripts

--- dev/launch_arbiter.sh
+++ dev/launch_arbiter.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 #
 # Copyright (C) 2015-2016: Alignak team, see AUTHORS.txt file for contributors
 #
@@ -17,35 +18,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Alignak.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-# This file incorporates work covered by the following copyright and
-# permission notice:
-#
-#  Copyright (C) 2009-2014:
-#     Gabes Jean, naparuba@gmail.com
-#     Gerhard Lausser, Gerhard.Lausser@consol.de
-#     Gregory Starck, g.starck@gmail.com
-#     Hartmut Goebel, h.goebel@goebel-consult.de
-#
-#  This file is part of Shinken.
-#
-#  Shinken is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Shinken is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
-
-
 DIR="$(cd $(dirname "$0"); pwd)"
-BIN="$DIR"/../alignak/bin
-ETC="$DIR"/../etc
 
-echo "Launching Arbiter (which reads configuration and dispatches it)"
-"$BIN"/alignak_arbiter.py -d -c "$ETC"/daemons/arbiterd.ini -a "$ETC"/alignak.cfg
+DAEMON_TYPE="ARBITER"
+DAEMON_NAME="${DAEMON_TYPE}_MASTER"
+
+"$DIR/_launch_daemon.sh" $@ -a "$DAEMON_NAME"
+
--- dev/stop_all.sh
+++ dev/stop_all.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 #
 # Copyright (C) 2015-2016: Alignak team, see AUTHORS.txt file for contributors
 #
@@ -17,38 +18,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Alignak.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
-# This file incorporates work covered by the following copyright and
-# permission notice:
-#
-#  Copyright (C) 2009-2014:
-#     Gabes Jean, naparuba@gmail.com
-#     Gerhard Lausser, Gerhard.Lausser@consol.de
-#     Gregory Starck, g.starck@gmail.com
-#     Hartmut Goebel, h.goebel@goebel-consult.de
-#
-#  This file is part of Shinken.
-#
-#  Shinken is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Shinken is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
-
-
 DIR="$(cd $(dirname "$0"); pwd)"
 
-
+"$DIR"/stop_arbiter.sh
 "$DIR"/stop_poller.sh
 "$DIR"/stop_reactionner.sh
 "$DIR"/stop_broker.sh
 "$DIR"/stop_receiver.sh
 "$DIR"/stop_scheduler.sh
-"$DIR"/stop_arbiter.sh
