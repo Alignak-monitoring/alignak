9a8b1a7760f13c02b0c8f916ca3114f69cac3d99
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-03-24 10:08:11 +0100

    Closes #765 - set_permissions.sh script allow to define user account

--- dev/set_permissions.sh
+++ dev/set_permissions.sh
@@ -19,27 +19,34 @@
 # along with Alignak.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# Default is alignak account
+ACCOUNT=$1
+# Parse command line arguments
+if [ $# -eq 0 ]; then
+    ACCOUNT="alignak"
+fi
+
 ## Same procedure as the one done in the debian installation
 ## Create user and group
-echo "Checking / creating 'alignak' user and users group"
-# Note: if the user exists, it's properties won't be changed (gid, home, shell)
-adduser  --quiet --system --home /var/lib/alignak --no-create-home --group alignak || true
+echo "Checking / creating '$ACCOUNT' user and users group"
+# Note: if the user exists, its properties won't be changed (gid, home, shell)
+adduser --quiet --system --home /var/lib/$ACCOUNT --no-create-home --group $ACCOUNT || true
 
 ## Create nagios group
 echo "Checking / creating 'nagios' users group"
 addgroup --system nagios || true
 
-## Add alignak to nagios group
-id -Gn alignak |grep -E '(^|[[:blank:]])nagios($|[[:blank:]])' >/dev/null ||
-    echo "Adding user 'alignak' to the nagios users group"
-    adduser alignak nagios
+## Add user to nagios group
+id -Gn $ACCOUNT |grep -E '(^|[[:blank:]])nagios($|[[:blank:]])' >/dev/null ||
+    echo "Adding user '$ACCOUNT' to the nagios users group"
+    adduser $ACCOUNT nagios
 
 ## Create directories with proper permissions
 for i in /usr/local/etc/alignak /usr/local/var/run/alignak /usr/local/var/log/alignak /usr/local/var/lib/alignak /usr/local/var/libexec/alignak
 do
     mkdir -p $i
-    echo "Setting 'alignak' ownership on: $i"
-    chown -R alignak:alignak $i
+    echo "Setting '$ACCOUNT' ownership on: $i"
+    chown -R $ACCOUNT:$ACCOUNT $i
 done
 
 echo "Setting file permissions on: /usr/local/etc/alignak"
