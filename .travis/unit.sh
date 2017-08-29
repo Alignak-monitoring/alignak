03d4d10fb53437199aeb03322d60463473cd8806
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-08-29 09:01:39 +0200

    Report missing lines in code coverage report

--- .travis/unit.sh
+++ .travis/unit.sh
@@ -9,7 +9,7 @@ coverage erase
 # Run test suite with py.test running its coverage plugin
 # Verbose mode to have the test list
 # Dump the 10 slowest tests
-pytest -v --durations=10 --cov=alignak --cov-config .coveragerc test_*.py
+pytest -v --durations=10 --cov=alignak --cov-report term-missing --cov-config .coveragerc test_*.py
 
 # Report about coverage
 coverage report -m
