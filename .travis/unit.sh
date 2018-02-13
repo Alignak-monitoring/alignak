3e2e5bdc4f36dffb53760af0f6b903fc3a1994ef
Frédéric MOHIER <frederic.mohier@gmail.com>
2018-02-13 05:33:39 +0100

    Fix broken requirement: - CherryPy must be <9.0 ! - Python 2.6 requirements!

--- .travis/unit.sh
+++ .travis/unit.sh
@@ -3,15 +3,16 @@
 set -ev
 
 cd test
-# Delete previously existing coverage results
-coverage erase
 
-# Run test suite with py.test running its coverage plugin
-# Verbose mode to have the test list
-# Dump the 10 slowest tests
-pytest --durations=10 --cov=alignak --cov-report term-missing --cov-config .coveragerc test_*.py
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    # Run test suite with py.test running its coverage plugin
+    # Dump the 10 slowest tests
+    pytest --durations=10 --cov=alignak --cov-report term-missing --cov-config .coveragerc test_*.py
+    # Report about coverage
+    coverage report -m
+else
+    pytest --durations=10 test_*.py
+fi
 
-# Report about coverage
-coverage report -m
 cd ..
 
